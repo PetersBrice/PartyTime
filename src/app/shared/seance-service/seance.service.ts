98465ab8c786cc0ea944db7521a6e1c5a595229e
brice <brice.peters@laposte.net>
2017-12-06 13:46:52 +0100

    néttoyage service

--- src/app/shared/seance-service/seance.service.ts
+++ src/app/shared/seance-service/seance.service.ts
@@ -10,11 +10,9 @@ import 'rxjs/add/operator/filter';
 export class SeanceService {
   // private property to store all backend URLs
   private _backendURL: any;
-  private temp: any;
 
   constructor(private _http: HttpClient) {
     this._backendURL = {};
-  this.temp = {};
     // build backend base url
     let baseUrl = `${environment.backend.protocol}://${environment.backend.host}`;
     if (environment.backend.port) {
@@ -31,33 +29,18 @@ export class SeanceService {
       .defaultIfEmpty([]);
   }
 
-  fetchRandom(): Observable<any> {
-    return this._http.get(this._backendURL.randomPeople, this._options())
-      .filter(_ => !!_)
-      .defaultIfEmpty({});
-  }
 
   fetchOne(id: string): Observable<any> {
     return this._http.get(this._backendURL.oneSeance.replace(':id', id), this._options());
   }
 
   create(seance: any): Observable<any> {
-    this.temp.nom = seance.nom;
-    this.temp.type = seance.type;
-    this.temp.description = seance.description;
-    this.temp.tel = seance.tel;
-    return this._http.post(this._backendURL.allSeance, this.temp, this._options());
+    const temp = {nom: seance.nom, type: seance.type, description: seance.description, tel: seance.tel};
+    return this._http.post(this._backendURL.allSeance, temp, this._options());
   }
 
   update(seance: any): Observable<any> {
-
-    console.log(seance);
-    /*on ne doit envoyer que le body*/
-    this.temp.nom = seance.nom;
-    this.temp.type = seance.type;
-    this.temp.description = seance.description;
-    this.temp.tel = seance.tel;
-    return this._http.put(this._backendURL.oneSeance.replace(':id', seance.id), this.temp, this._options());
+    return this._http.put(this._backendURL.oneSeance.replace(':id', seance.id), seance, this._options());
   }
 
   delete(id: string): Observable<any[]> {
